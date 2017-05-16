650d983dc40fa10f44d7100df8538b94976f84b3
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-05-16 03:49:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function takeANumber (katzDeliLine, personName){
+  katzDeliLine.push(personName);
+  return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length===0){
+    return `There is nobody waiting to be served!`;
+  }
+  var firstPerson=katzDeliLine.shift();
+  return `Currently serving ${firstPerson}.`
+}
+
+function currentLine(katzDeliLine){
+for (var i=0;i<katzDeliLine.length;i++){
+  var string= `The line is currently`
+  if (katzDeliLine.length===0)
+  return `The line is currently empty.`;
+  return `The line is currently ${katzDeliLine.length}`
+}
+
+}
